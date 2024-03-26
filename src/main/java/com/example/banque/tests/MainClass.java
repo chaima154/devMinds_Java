c90c920b25f3ff90e942c4188f9dab5fd3a1eb03
chaima154 <116753982+chaima154@users.noreply.github.com>
2024-03-26 13:00:14 +0100

    Initial commit

---
+++ src/main/java/com/example/banque/tests/MainClass.java
@@ -0,0 +1,9 @@
+package com.example.banque.tests;
+
+import com.example.banque.tools.MyConnection;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyConnection mc = new MyConnection();
+    }
+}
