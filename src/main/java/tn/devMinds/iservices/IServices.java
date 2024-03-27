ff700c96ec61bc12d16443a3fee51b7e847dccf5
hazemkammoun <hazem.kammoun@esprit.tn>
2024-03-27 15:59:12 +0100

    correct version

---
+++ src/main/java/tn/devMinds/iservices/IServices.java
@@ -0,0 +1,12 @@
+package tn.devMinds.iservices;
+import java.sql.SQLException;
+import java.util.ArrayList;
+public interface IServices <T>{
+
+        ArrayList<T> getAll() throws SQLException;
+        boolean add(T t) throws SQLException;
+        boolean delete(T t) throws SQLException;
+        boolean delete(int id) throws SQLException;
+        boolean update(T t) throws SQLException;
+}
+
