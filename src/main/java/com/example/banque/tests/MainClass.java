29e99c96bce4a7db95c0941bc6e575c2f334a6ce
hazemkammoun <hazem.kammoun@esprit.tn>
2024-03-27 15:37:09 +0100

    ...

--- src/main/java/com/example/banque/tests/MainClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.banque.tests;
-
-import com.example.banque.tools.MyConnection;
-
-public class MainClass {
-    public static void main(String[] args) {
-        MyConnection mc = new MyConnection();
-    }
-}
--- src/main/java/tn/devMinds/tools/MyConnection.java
+++ src/main/java/tn/devMinds/tools/MyConnection.java
@@ -1,4 +1,4 @@
-package com.example.banque.tools;
+package tn.devMinds.tools;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
