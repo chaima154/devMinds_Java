badb567cfc080a12e6e3d6c1f511a92d0c9bb673
chaima154 <116753982+chaima154@users.noreply.github.com>
2024-04-16 21:51:01 +0200

    commit le 16/04

--- src/main/java/tn/devMinds/controllers/AjouterTransactionController.java
+++ src/main/java/tn/devMinds/controllers/AjouterTransactionController.java
@@ -3,8 +3,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
+import javafx.scene.layout.VBox;
 import tn.devMinds.entities.Transaction;
 import tn.devMinds.entities.TypeTransaction;
 import tn.devMinds.iservices.TransactionService;
