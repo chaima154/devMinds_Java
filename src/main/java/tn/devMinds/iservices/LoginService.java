e370da9eb205d0edcf356b5b39d45b82f5fec0e5
fatmaZine <103320287+fatmaZine@users.noreply.github.com>
2024-05-06 21:01:43 +0100

    hachage complet

--- src/main/java/tn/devMinds/iservices/LoginService.java
+++ src/main/java/tn/devMinds/iservices/LoginService.java
@@ -2,6 +2,7 @@ package tn.devMinds.iservices;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import org.mindrot.jbcrypt.BCrypt;
 import tn.devMinds.tools.MyConnection;
 
 import java.sql.Connection;
@@ -27,17 +28,21 @@ public class LoginService extends Service<Boolean> {
             @Override
             protected Boolean call() throws Exception {
                 // Créer une requête SQL en fonction du rôle
-                String query = "SELECT * FROM user WHERE email = ? AND mdp = ? AND role = ?";
+                String query = "SELECT mdp FROM user WHERE email = ? AND role = ?";
                 try (Connection connection = MyConnection.getConnection();
                      PreparedStatement preparedStatement = connection.prepareStatement(query)) {
                     preparedStatement.setString(1, email);
-                    preparedStatement.setString(2, mdp);
-                    preparedStatement.setString(3, role);
+                    preparedStatement.setString(2, role);
 
                     // Exécuter la requête
                     try (ResultSet resultSet = preparedStatement.executeQuery()) {
                         // Vérifier si l'utilisateur existe dans la base de données
-                        return resultSet.next(); // true si l'utilisateur existe, sinon false
+                        if (resultSet.next()) {
+                            String hashedPassword = resultSet.getString("mdp");
+                            // Vérifier si le mot de passe fourni correspond au mot de passe haché dans la base de données
+                            return BCrypt.checkpw(mdp, hashedPassword);
+                        }
+                        return false; // Aucun utilisateur trouvé
                     }
                 } catch (SQLException e) {
                     System.out.println("Erreur lors de l'authentification: " + e.getMessage());
