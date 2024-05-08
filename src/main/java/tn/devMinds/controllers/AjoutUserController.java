599d6c7a7d0cbd78af5ec7232da4090229f7a606
fatmaZine <103320287+fatmaZine@users.noreply.github.com>
2024-05-08 03:23:18 +0100

    complexity

--- src/main/java/tn/devMinds/controllers/AjoutUserController.java
+++ src/main/java/tn/devMinds/controllers/AjoutUserController.java
@@ -5,16 +5,20 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import tn.devMinds.entities.User;
 import tn.devMinds.entities.Role;
 import tn.devMinds.iservices.UserService;
 import org.mindrot.jbcrypt.BCrypt;
+import tn.devMinds.controllers.PasswordComplexityCalculator;
 
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AjoutUserController extends BackendHome {
+    @FXML
+    private Label complexityLabel;
     @FXML
     private VBox container;
     @FXML
@@ -39,6 +43,22 @@ public class AjoutUserController extends BackendHome {
     private Label mdpError;
 
 
+//    private void displayPasswordComplexity() {
+//        String password = passwordField.getText();
+//        int complexity = PasswordComplexityCalculator.calculateComplexity(password);
+//        String message;
+//        if (complexity >= 80) {
+//            message = "Complexité du mot de passe : Forte";
+//        } else if (complexity >= 50) {
+//            message = "Complexité du mot de passe : Moyenne";
+//        } else {
+//            message = "Complexité du mot de passe : Faible";
+//        }
+//        // Display the complexity message as a notification
+//        // You can use a Label or other UI element to display the message
+//        complexityLabel.setText(message);
+//    }
+
 
     // Méthode appelée lorsque le bouton "Ajouter" est cliqué
     public void addUser() throws SQLException {
@@ -47,7 +67,35 @@ public class AjoutUserController extends BackendHome {
         String prenom = lastNameField.getText();
         String email = emailField.getText();
         String mdp = passwordField.getText();
-        String selectedRoleString = String.valueOf(roleComboBox.getValue());  // Get the selected role as a String
+        String selectedRoleString = String.valueOf(roleComboBox.getValue());
+
+        int complexity = PasswordComplexityCalculator.calculateComplexity(mdp);
+        String complexityNotification = "";
+
+        switch (complexity) {
+            case 1:
+                complexityNotification = "Faible";
+                break;
+            case 2:
+                complexityNotification = "Moyenne";
+                break;
+            case 3:
+                complexityNotification = "Bonne";
+                break;
+            case 4:
+                complexityNotification = "Très bonne";
+                break;
+            case 5:
+                complexityNotification = "Excellente";
+                break;
+            default:
+                complexityNotification = "Inconnue";
+                break;
+        }
+
+        System.out.println("Complexité du mot de passe: " + complexityNotification);
+
+        // Get the selected role as a String
         String hashedPassword = BCrypt.hashpw(mdp, BCrypt.gensalt());
 
         // Convert the selected role String to Role enum
@@ -129,4 +177,6 @@ public class AjoutUserController extends BackendHome {
             // Error occurred while adding the user
             System.out.println("Erreur lors de l'ajout de l'utilisateur: " + result);
         }
-    }}
+        Notification.display("Complexité du mot de passe: ", complexityNotification);
+    }
+}
